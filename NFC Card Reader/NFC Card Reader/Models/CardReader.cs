e80ab740bb119a36698258c349a37ec04c6823e7
aboyce <apboyce@outlook.com>
2015-07-23 13:46:56 +0100

    *Test commit on new branch. *Added Models folder. *Moved Configuration to Models Folder. *Added CardReader to Models folder.

---
+++ NFC Card Reader/NFC Card Reader/Models/CardReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFC_Card_Reader.Models
+{
+    public class CardReader
+    {
+    }
+}
