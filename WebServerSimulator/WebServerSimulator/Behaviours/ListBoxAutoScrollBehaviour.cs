5088cdca41670dcd262392e6a51100080f52d1d5
Adam Boyce <apboyce@outlook.com>
2015-08-18 21:08:12 +0100

    *Started to implement an AutoScroll behaviour for the Logging List Box.

---
+++ WebServerSimulator/WebServerSimulator/Behaviours/ListBoxAutoScrollBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+
+namespace WebServerSimulator.Behaviours
+{
+    public class ListBoxAutoScrollBehaviour : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            ListBox listBox = AssociatedObject;
+            ((INotifyCollectionChanged)listBox.Items).CollectionChanged += OnListBoxCollectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            ListBox listBox = AssociatedObject;
+            ((INotifyCollectionChanged)listBox.Items).CollectionChanged -= OnListBoxCollectionChanged;
+        }
+
+        private void OnListBoxCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            ListBox listBox = AssociatedObject;
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                listBox.ScrollIntoView(e.NewItems[0]);
+            }
+        }
+    }
+}
